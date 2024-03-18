702849df26cf6f6163802c2fff42f5135ca4abd0
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-17 21:04:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit as st
 import plotly.express as px
 
 # Importando os dados que estao em parquet
-dados = pd.read_parquet('/content/drive/MyDrive/PySUS/ANIMBR07_22.parquet.gzip')
+dados = pd.read_parquet('https://drive.google.com/uc?export=download&id=1Fp0ndVsc49aZB1LhuKL0ivZ1PY2592GN')
 
 st.dataframe(dados)
