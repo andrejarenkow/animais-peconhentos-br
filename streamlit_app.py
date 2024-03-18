263aa42d1af182f71e856528473d28464eb2da22
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-03-17 21:15:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import plotly.express as px
 @st.cache_data
 def load_data(persist=True):
   # Importando os dados que estao em parquet
-    data = pd.read_parquet('https://drive.google.com/uc?export=download&id=1Fp0ndVsc49aZB1LhuKL0ivZ1PY2592GN')
+    data = pd.read_parquet('https://drive.google.com/uc?export=download&id=1-4ZSiWudlwmC0kCHDCX46qPp5RGNwT4X')
     return data
 
 
